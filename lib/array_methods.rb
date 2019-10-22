3eeb650bf64117926791687b8c5f91ec6bcc8a22
brad hemmerick <brad.hemmerick13@gmail.com>
2019-10-22 23:03:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,12 +8,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-    counter = 0
- 
-  while array[counter] do
-    puts array[counter]
-    counter += 1
-  end
+  array.find_max_value
 end
 
 def find_min_value(array)
